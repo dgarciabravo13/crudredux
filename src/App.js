d08e1d99bd76e6e66b16d25cc7e0d70456a1ecf9
David Garcia <dgarciabravo13@gmail.com>
2020-11-26 11:25:20 +0100

    create Form in new Product

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Header from "./components/Header";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Productos from "./components/Productos";
+import NuevoProducto from "./components/NuevoProducto";
+import EditarProducto from "./components/EditarProducto";
 
 function App() {
   return (
-    <>
-    <Header></Header>
-    </>
+    <Router>
+    <Header/>
+    <div className="container">
+      <Switch>
+        <Route exact path="/" component={Productos}/>
+        <Route exact path="/productos/nuevo" component={NuevoProducto}/>
+        <Route exact path="/productos/editar/:id" component={EditarProducto}/>
+      </Switch>
+    </div>
+    </Router>
   );
 };
 
